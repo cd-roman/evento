9ee8bd90bf17a710b9ea688d9f07f1b4d2486e8a
Roman <60399380+cd-roman@users.noreply.github.com>
2024-05-02 01:17:26 +0200

    Update next.config.js, layout.tsx, page.tsx, header.tsx, and add logo.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,41 @@
+"use client";
+
 import Link from "next/link";
+import Logo from "./logo";
+import { usePathname } from "next/navigation";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
 
 export default function Header() {
+  const activePathname = usePathname();
+
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+    <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
+      <Logo />
+
+      <nav className="h-full">
+        <ul className="flex gap-x-6 h-full text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className={
+                "hover:text-white flex items-center relative transition"
+              }
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
