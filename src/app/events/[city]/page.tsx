3b7593d81f836f66960f766afe044210d9f33644
Roman <60399380+cd-roman@users.noreply.github.com>
2024-05-03 05:27:21 +0200

    Create EventCard and EventList components

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,6 +1,7 @@
-import { EventoEvent } from "@/src/lib/types";
-import H1 from "../../../components/H1";
-import { capitalize } from "../../../lib/utils";
+import { EventoEvent } from "@/lib/types";
+import H1 from "@/components/H1";
+import { capitalize } from "@/lib/utils";
+import EventsList from "@/components/events-list";
 
 type Props = {
   params: {
@@ -9,20 +10,18 @@ type Props = {
 };
 
 export default async function EventsPage({ params }: Props) {
-  const city = params.city;
-
   const response = await fetch(
     "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
   );
   const events: EventoEvent[] = await response.json();
 
+  const city = params.city;
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>{capitalize(city)}</H1>
 
-      {events.map((event) => (
-        <section key={event.id}>{event.name}</section>
-      ))}
+      <EventsList events={events} />
     </main>
   );
 }
