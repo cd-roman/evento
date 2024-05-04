dc028664d81bafdccfab905484c12f308678ba58
Roman <60399380+cd-roman@users.noreply.github.com>
2024-05-05 00:24:56 +0200

    Add pagination component and logic for the Events page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -11,6 +11,10 @@ type Props = {
   };
 };
 
+type EventsPageProps = Props & {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
 export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
 
@@ -19,15 +23,19 @@ export function generateMetadata({ params }: Props): Metadata {
   };
 }
 
-export default async function EventsPage({ params }: Props) {
+export default async function EventsPage({
+  params,
+  searchParams,
+}: EventsPageProps) {
   const city = params.city;
+  const page = Number(searchParams.page) || 1;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">Events in {capitalize(city)}</H1>
 
-      <Suspense fallback={<Loading />}>
-        <EventsList city={city} />
+      <Suspense key={city + page} fallback={<Loading />}>
+        <EventsList city={city} page={page} />
       </Suspense>
     </main>
   );
